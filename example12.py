6217971121bd3f1a5bc343798226f4d97583a778
vgvr0 <126200367+vgvr0@users.noreply.github.com>
2024-05-05 00:29:17 +0200

    Create example12.py

---
+++ example12.py
@@ -0,0 +1,22 @@
+import asyncio
+
+async def tarea1():
+    await asyncio.sleep(1)  # Simula una operación I/O
+    return 'Resultado de tarea1'
+
+async def tarea2():
+    await asyncio.sleep(2)  # Simula una operación I/O
+    return 'Resultado de tarea2'
+
+async def main():
+    resultado1, resultado2 = await asyncio.gather(tarea1(), tarea2())
+    print(resultado1)
+    print(resultado2)
+
+# Python 3.7+
+asyncio.run(main())
+
+# Para Python 3.6
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(main())
+# loop.close()
